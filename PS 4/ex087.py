666343fb997642a8642a136da4d39f02e4093f00
DGTV11 <52272655+DGTV11@users.noreply.github.com>
2023-12-11 12:30:32 +0800

    Delete PS 4 directory

--- PS 4/ex087.py
+++
@@ -1,9 +0,0 @@
-BASE_COST = 10.95
-ADDITIONAL_COSTS = 2.95
-
-def calculate(n):
-    cost = BASE_COST + ((n-1) * ADDITIONAL_COSTS)
-    return cost
-
-if __name__ == "__main__":
-    print("Cost: $%s"%calculate(float(input("Number of items? "))))
--- PS 4/ex096b.py
+++
@@ -1,9 +0,0 @@
-def isInteger(_str):
-    s = (_str.strip()).lstrip("-")
-    return s.isdigit()
-
-if __name__ == "__main__":
-    if isInteger(input("Please input something (numbers are preferred): ")):
-        print("Input represents an integer!")
-    else:
-        print("Input does not represent an integer!")
