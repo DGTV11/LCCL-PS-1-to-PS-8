416ada9cf4cd06f4c803dd0b060ee945c4eb1703
DGTV11 <52272655+DGTV11@users.noreply.github.com>
2023-12-11 12:30:41 +0800

    Delete PS 5 directory

--- PS 5/ex127.py
+++
@@ -1,8 +0,0 @@
-from inputs import inpList
-
-def isSorted(l):
-    return l == sorted(l)
-
-if __name__ == '__main__':
-    l = inpList("Pick a float, any float (type nothing and enter to quit): ", "float", "")
-    print(isSorted(l))
